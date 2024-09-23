32859083f013c7a314b44f918d72b3e1cd9898fb
Akira Hayakawa <ruby.wktk@gmail.com>
2024-09-23 22:44:10 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use std::time::Duration;
-use tracing::instrument::WithSubscriber;
-use tracing::{info, span, subscriber::set_default, Level};
 use anyhow::Result;
-use tracing_futures::{Instrument};
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::{layer, Layer};
-use tracing::{Event, Subscriber};
-use tracing_subscriber::{fmt, layer::Context, Registry};
-use tracing_subscriber::fmt::format::{self, Format, Writer};
-use tracing_subscriber::prelude::*;
+use tracing::dispatcher::set_global_default;
+use tracing::instrument::WithSubscriber;
+use std::time::Duration;
+use tracing::{info, subscriber::set_default};
+use tracing_subscriber::fmt::{self, layer};
+use tracing_subscriber::fmt::format::Writer;
+use tracing_subscriber::fmt::FormatEvent;
 use tracing_subscriber::registry::LookupSpan;
-use tracing_subscriber::fmt::{FormatEvent, FormatFields};
+use tracing::Dispatch;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
 mod proto {
     tonic::include_proto!("my_service");
 }
+
 #[derive(Debug)]
 struct App;
 #[tonic::async_trait]
@@ -25,55 +25,20 @@ impl proto::my_service_server::MyService for App {
     }
 }
 
-struct CustomLayer;
-impl<S> tracing_subscriber::Layer<S> for CustomLayer
-where
-    S: tracing::Subscriber,
-{
-    fn on_event(
-        &self,
-        event: &tracing::Event<'_>,
-        _ctx: tracing_subscriber::layer::Context<'_, S>,
-    ) {
-        println!("ND>");
-    }
-}
-
-// pub struct PrefixFormatter;
-
-// impl<S, N> tracing_subscriber::fmt::FormatEvent<S, N> for PrefixFormatter
-// where
-//     S: tracing::Subscriber + for<'a> LookupSpan<'a>,
-//     N: for<'a> tracing_subscriber::fmt::FormatFields<'a> + 'static,
-// {
-//     fn format_event(
-//         &self,
-//         ctx: &fmt::FmtContext<'_, S, N>,
-//         mut writer: Writer<'_>,
-//         event: &tracing::Event<'_>,
-//     ) -> std::fmt::Result {
-//         // ログの前に「ND1>」を付加
-//         write!(writer, "ND1> ")?;
-//         // 標準のログフォーマットを適用
-//         ctx.format_fields(writer.by_ref(), event)?;
-//         writeln!(writer)
-//     }
-// }
-
 pub struct PrefixedFormatter<F> {
-    inner: F, // Delegate先のフォーマッタ
-    prefix: &'static str,
+    inner: F,
+    prefix: String,
 }
 
 impl<F> PrefixedFormatter<F> {
-    pub fn new(inner: F, prefix: &'static str) -> Self {
+    pub fn new(inner: F, prefix: String) -> Self {
         Self { inner, prefix }
     }
 }
 
 impl<S, N, F> FormatEvent<S, N> for PrefixedFormatter<F>
 where
-    S: tracing::Subscriber + for <'a> LookupSpan<'a>,
+    S: tracing::Subscriber + for<'a> LookupSpan<'a>,
     N: for<'a> tracing_subscriber::fmt::FormatFields<'a> + 'static,
     F: FormatEvent<S, N>, // Delegate先のフォーマッタが FormatEvent を実装している
 {
@@ -83,58 +48,62 @@ where
         mut writer: Writer<'_>,
         event: &tracing::Event<'_>,
     ) -> std::fmt::Result {
-        // ログの前に prefix を出力
         write!(writer, "{}> ", self.prefix)?;
-        // Delegate先のフォーマッタに処理を委譲
         self.inner.format_event(ctx, writer, event)
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
     let mut free_ports = vec![];
-    for _ in 0..20 {
+    for _ in 0..10 {
         let p = port_check::free_local_ipv4_port().unwrap();
         free_ports.push(p);
     }
     dbg!(&free_ports);
 
     for port in free_ports.clone() {
-        let format = tracing_subscriber::fmt::format().with_target(false).compact();
-        let format = PrefixedFormatter::new(format, "ND1");
-        // let sub0 = tracing_subscriber::fmt::layer().event_format();
-        let sub = tracing_subscriber::fmt().event_format(format).finish();
-        // let sub = tracing_subscriber::registry().with(CustomLayer).with_subscriber(sub).into_inner();
-        // let sub = sub.with(CustomLayer);
-        // let l = CustomLayer;
-        // let l2 = l.and_then(sub);
-
-        // let sub = tracing_subscriber::Registry::default().with(CustomLayer).with_subscriber(sub).into_inner();
+        let format = tracing_subscriber::fmt::format()
+            .with_target(false)
+            .compact();
+        let nd_number = format!("ND{port}");
+    
 
         let svc_task = async move {
-            let _g = set_default(sub);
+            // let _g = set_default(sub);
 
             let svc = proto::my_service_server::MyServiceServer::new(App);
             let socket = format!("0.0.0.0:{port}").parse().unwrap();
             tonic::transport::Server::builder()
                 .add_service(svc)
-                .serve(socket).await.unwrap();
+                .serve(socket)
+                .await
+                .unwrap();
         };
-        tokio::spawn(svc_task);
+        // tokio::spawn(svc_task);
+
+        std::thread::spawn(move || {
+            let format = PrefixedFormatter::new(format, nd_number);
+            let sub = tracing_subscriber::fmt().event_format(format).finish();
+            let _g = set_default(sub);
+            let runtime = tokio::runtime::Runtime::new().unwrap();
+            runtime.block_on(svc_task);
+        });
     }
 
-    tokio::time::sleep(Duration::from_secs(1)).await;
+    tokio::time::sleep(Duration::from_secs(5)).await;
 
-    use rand::seq::SliceRandom;
-    use rand::thread_rng;
     let mut rng = thread_rng();
-    for _ in 0..20 {
+    for _ in 0..10 {
         let tgt_port = free_ports.choose(&mut rng).unwrap();
-        dbg!(tgt_port);
-        let mut cli = proto::my_service_client::MyServiceClient::connect(format!("http://localhost:{tgt_port}")).await?;
+        // dbg!(tgt_port);
+        let mut cli = proto::my_service_client::MyServiceClient::connect(format!(
+            "http://localhost:{tgt_port}"
+        ))
+        .await?;
         cli.ping(()).await?;
     }
 
+    eprintln!("done");
     Ok(())
 }
